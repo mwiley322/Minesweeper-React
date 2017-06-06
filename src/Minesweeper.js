edc2ab9c4ca05b0a39cddfde9823add417fc7306
Megan Wiley <mwiley322@gmail.com>
2017-06-06 08:44:27 -0700

    Initial commit

---
+++ src/Minesweeper.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export default class Minesweeper extends Component {
+  render() {
+    return (
+      <h1>Hello, World</h1>
+    );
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Minesweeper from './Minesweeper';
+import './index.css';
+
+ReactDOM.render(<Minesweeper />, document.getElementById('root'));
